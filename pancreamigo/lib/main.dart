2d188e12726809909601d644f46344ee35a666a8
Liander <liander.medeiros@gmail.com>
2023-05-07 11:52:38 -0300

    :construction: Teste do workflow de deploy na play store

--- pancreamigo/lib/main.dart
+++ pancreamigo/lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Você clicou no botão essa quantidade de vezes:',
             ),
             Text(
               '$_counter',
